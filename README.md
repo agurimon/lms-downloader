e6963fd6cc055fbf62368c690645cf05dcca2cce
agurimon <agurimon18@gmail.com>
2023-06-18 04:43:12 +0900

    init

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # LMS Downloader
 
-숭실대학교 LMS 동영상 다운로더
+LMS 동영상 강의 페이지에서 mp4 주소를 파싱하여 curl 명령어로 만들어주는 프로그램입니다.
 
-![1](./images/1.png)
+## Example
+프로그램으로 사용해서 얻을 수 있는 결과입니다.
+
 ![1](./images/2.png)
 ![1](./images/3.png)
+
+```
+curl --output '[OS-P] 7_fs1_0.mp4' --location 'https://ssuin-object.commonscdn.com/ssu-contents/contents/ssu1000001/628e240000000/contents/media_files/screen.mp4' --header 'Range: bytes=0-' --header 'Referer: https://commons.ssu.ac.kr/'
+```
+
+## Using
+1. 동영상강의 페이지에서 새로 생긴 동영상 다운로드 버튼 클릭
+2. textarea에 생긴 링크들 복사
+3. 터미널에 붙여넣기
+
+## Modify
+귀찮다면 bundle.js만 고치세요.
+
+### Install
+```
+npm init -y
+npm install xml2js browserify --save
+```
+
+### Build
+```
+npx browserify background.js -o bundle.js
+```
--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var xml2js = require('xml2js');
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log('Received message:', request);
     if (request.message === 'sendMessage') {
@@ -25,27 +27,27 @@ async function fetchResource(url) {
 
 async function getfilename(contentId) {
     const xmlUrl = `https://commons.ssu.ac.kr/viewer/ssplayer/uniplayer_support/content.php?content_id=${contentId}`;
-
+    console.log(xmlUrl)
     try {
         const data = await (await fetchResource(xmlUrl)).text();
-        const xmlDoc = new DOMParser().parseFromString(data, 'application/xml');
-        const content_type = xmlDoc.querySelector('content_type').textContent;
+        var parser = new xml2js.Parser();
+        const result = await parser.parseStringPromise(data);
 
-        if (content_type === "upf") {
-            let media_uri = xmlDoc.querySelector('media_uri').textContent;
-            const _filenames = xmlDoc.querySelectorAll('main_media');
-            
-            let urls = [];
-            _filenames.forEach(filename => {
-                urls.push(media_uri.replace("[MEDIA_FILE]", filename.textContent));
-            });
-
-            return urls;
+        console.log(result.content.content_playing_info[0].content_type)
+        if (result.content.content_playing_info[0].content_type[0] === "upf") {
+          const stories = result.content.content_playing_info[0].story_list[0].story;
+          const media_ids = stories.map(story => story.main_media_list[0].main_media[0]._);
+          const media_uri = result.content.service_root[0].media[0].media_uri[0]._;
+  
+          let urls = [];
+          media_ids.forEach(media_id => {
+              urls.push(media_uri.replace("[MEDIA_FILE]", media_id));
+          });
+  
+          return urls;
         } else {
-            const media_uri = xmlDoc.querySelector('media_uri').textContent;
-            return media_uri
+          return result.content.content_playing_info[0].main_media[0].desktop[0].html5[0].media_uri[0];
         }
-
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -54,30 +56,6 @@ async function getfilename(contentId) {
 async function download(contentId, title) {
     const urls = await getfilename(contentId);
     
-    // try {
-    //     const proxyLink = 'http://localhost:3000/proxy?url=';
-    //     const response = await fetchResource(proxyLink + urls[0]);
-    //     const contentLength = parseInt(response.headers.get('Content-Length'), 10);
-    //     const reader = response.body.getReader();
-
-    //     const stream = new ReadableStream({
-    //         async start(controller) {
-    //             while (true) {
-    //                 const { done, value } = await reader.read();
-    //                 if (done) {
-    //                     controller.close();
-    //                     break;
-    //                 }
-    //                 controller.enqueue(value);
-    //             }
-    //         }
-    //     });
-
-    //     const buffer = await (await new Response(stream)).arrayBuffer();
-    //     saveAsFile(buffer, `${title}.mp4`);
-    // } catch (error) {
-    //     throw new Error('Error fetching data:', error);
-    // }
     return {'urls': urls, 'title': title};
 }
 
